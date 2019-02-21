432b3c8364d17dd0cc95d45db1da1b8cdd949c0d
dengyouhao <dengyouhao123>
2019-02-21 16:31:23 +0800

    pos点餐系统

---
+++ AwesomePos/src/components/page/Pos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pos">
+    <el-row>
+      <el-col :span='7'>
+        我是订单栏
+      </el-col>
+      <el-col :span='7'>
+        我是产品栏
+      </el-col>
+
+    </el-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos'
+}
+</script>
